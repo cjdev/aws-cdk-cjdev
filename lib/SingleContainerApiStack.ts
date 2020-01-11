4cca1e1fc6a6b359513530c0e200eefbf75369e7
Windsor, Jeffrey <jwindsor@cj.com>
2020-01-11 11:16:10 -0800

    comments on single stack

--- lib/SingleContainerApiStack.ts
+++ lib/SingleContainerApiStack.ts
@@ -18,20 +18,23 @@ interface SingleContainerApiStackProps {
 
 //TODO: look into helping the wiring of the task executrion and task role, currently passed in with the taskProps
 
+/**
+ * Stack with all the components and wiring required to run a single container.
+ * Note: taskRunnerFunctionProps will have its role and environment properties overwritten for proper wiring
+ */
 class SingleContainerApiStack extends Stack {
     constructor(scope: Construct,
                 id: string,
                 props: SingleContainerApiStackProps) {
         super(scope, id);
 
-        const repo = new SingleImageRepository(this,'SingleImageRepository');
-        const repoUploadImageRole = new RepoUploaderRole(this, 'RepoUploaderRole', repo, props.repoUploadImageRoleAssumedByPrincipal);
+        const imageRepository = new SingleImageRepository(this,'SingleImageRepository');
+        const imageRepositoryUploadRole = new RepoUploaderRole(this, 'RepoUploaderRole', imageRepository, props.repoUploadImageRoleAssumedByPrincipal);
 
         const cluster = new Cluster(this, 'Cluster');
-        const task = new FargateContainerTaskDefinition(this, 'ContainerTask', repo.Image, props.containerEnvVars, props.taskProps);
-        const taskRunRole = new FargateTaskDefinitionRunTaskRole(this, 'RunTaskRole', task);
+        const task = new FargateContainerTaskDefinition(this, 'ContainerTask', imageRepository.Image, props.containerEnvVars, props.taskProps);
         const taskRunner = new Function(this, 'RunTaskLambda', {
-            role: taskRunRole,
+            role: new FargateTaskDefinitionRunTaskRole(this, 'RunTaskRole', task),
             environment: {
                 taskDefinitionArn: task.taskDefinitionArn,
                 clusterArn: cluster.clusterArn,
@@ -40,11 +43,10 @@ class SingleContainerApiStack extends Stack {
             },
             ...props.taskRunnerFunctionProps
         });
+        new RestApiProxyWithCustomAuthorizer(this,'RestApi',taskRunner, props.apiAuthorizerFunctionProps);
 
-        const api = new RestApiProxyWithCustomAuthorizer(this,'RestApi',taskRunner, props.apiAuthorizerFunctionProps);
-
-        new CfnOutput(this, 'SingleImageRepositoryUri', {value: repo.repositoryUri});
-        new CfnOutput(this, 'RepoUploaderRoleArn', {value: repoUploadImageRole.roleArn});
+        new CfnOutput(this, 'ImageRepositoryUri', {value: imageRepository.repositoryUri});
+        new CfnOutput(this, 'ImageRepositoryUploadRoleArn', {value: imageRepositoryUploadRole.roleArn});
         new CfnOutput(this, 'ContainerTaskDefinitionArn', {value: task.taskDefinitionArn});
     }
 }
