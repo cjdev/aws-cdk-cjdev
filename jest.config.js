a8396926bd6fb4379803db737f5f6a9c183c3bb2
Windsor, Jeffrey <jwindsor@cj.com>
2020-01-13 08:33:06 -0800

    Add Tests, but still weak support

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     "roots": [
-      "<rootDir>/test"
+        "<rootDir>/test"
     ],
     testMatch: [ '**/*.test.ts'],
     "transform": {
-      "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": "ts-jest"
     },
-  }
+}
